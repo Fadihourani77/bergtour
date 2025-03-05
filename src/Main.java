ab6b468a8f1997016fd3ce621754aa0425c3f333
Fadi <fadi.houran10@gmail.com>
2025-03-05 17:49:09 +0100

    add variable

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         int i = 1;
+        int x =0;
         if (i == 1) {
             System.out.println("first line");
         }
