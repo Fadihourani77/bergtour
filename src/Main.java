21c99bd197808916cbf6dfcc3975d79f7c8509dd
Fadi <fadi.houran10@gmail.com>
2025-03-05 17:30:39 +0100

    here is my secont update on code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("first line");
+        int i = 1;
+        if (i == 1) {
+            System.out.println("first line");
+        }
     }
 
 }
