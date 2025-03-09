6414205b530e84d8b41ddc82398220b206f1520f
Fadi <fadi.houran10@gmail.com>
2025-03-09 15:19:10 +0100

    changed the line

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, lili");
         int i = 1;
         int x =0;
         if (i == 1) {
             System.out.println("first line");
+
         }
     }
 
